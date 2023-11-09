1c11aa1378a4a1b0b9e838d843d6adf550d33944
hansellperez <112670508+hansellperez@users.noreply.github.com>
2023-11-08 17:42:57 -0800

    finished cp07 codes
    created code files for cp07 which i had originally saved in class09252023.m file

--- HansellPerezHeatBD1.m
+++ HansellPerezHeatBD1.m
@@ -1,14 +1,9 @@
-%% look at algorithm 12.2 in text book
-% check code with example 2 from sec. 12.2 (pg 731)
-% also talked about section 6.6
-%% Method 1
 function [wN] = HansellPerezHeatBD1(l, T, alpha, m, N)
     h = l/m;
     k = T/N;
     lambda = alpha^2*k/h^2;
-    A = diag(1+2*lambda) + diag(-lambda, 1) + diag(-lambda, -1);
+    A = diag((1+2*lambda)*ones(m-1,1)) + diag(-lambda*ones(m-2,1), 1) + diag(-lambda*ones(m-2,1), -1);
     x = 0:h:l;
-    t = 0:k:T;
     f = fm;
     w0 = zeros(m-1,1);
 
@@ -25,7 +20,3 @@ end
 function f = fm
      f = @(x) sin(pi*x); % insert some function of f
 end
-
-%% Method 2
-
-%% Method 3
